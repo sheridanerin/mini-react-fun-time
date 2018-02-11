8eeca1c8e07e8f57181e247809e485a810a79744
Erin Sheridan <esheridan@overstock.com>
2018-02-11 11:16:54 -0700

    added a form to the edit poage and got it working with state.  Still need to save items somewhere.

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import './Edit.css';
 
 export default class Edit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            position: ''
+        };
+    }
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="edit-container">
-                <span>Im the edit page!</span>
+                <h2>Edit Roster</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Player Name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        Player Position:
+                        <input
+                            type="text"
+                            name="position"
+                            value={this.state.Position}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <input
+                        type="submit"
+                        value="Submit"
+                    />
+                </form>
             </div>
         )
     }
