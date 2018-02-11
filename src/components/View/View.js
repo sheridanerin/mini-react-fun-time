05878427cb35fdc0fe6c431220bfeb2821072f12
Erin Sheridan <esheridan@overstock.com>
2018-02-11 12:16:27 -0700

    all added items/persons are visible on the view page now.

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './View.css';
 
 export default class View extends Component {
+
+    buildRosterList = () => {
+        const playersList = JSON.parse(localStorage.getItem('playersList'));
+        return playersList.map((player) => {
+            return (
+                <li className="player-line" key={player.name}>
+                    <span className="roster-entry-left">Name: {player.name}</span>
+                    <span className="roster-entry-right">Position: {player.position}</span>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
             <div className="view-container">
-                <span>Im the view container</span>
+                <h2>View Roster</h2>
+                <ul className="roster-list">
+                    {this.buildRosterList()}
+                </ul>
             </div>
         )
     }
