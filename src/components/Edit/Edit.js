097e4033414c2e54c4b9509fc6bba6872176cc62
Erin Sheridan <esheridan@overstock.com>
2018-02-11 22:18:17 -0700

    added player removal capability

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -9,6 +9,7 @@ export default class Edit extends Component {
             position: ''
         };
     }
+
     handleChange = (event) => {
         this.setState({[event.target.name]: event.target.value});
     }
--- src/components/View/View.js
+++ src/components/View/View.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './View.css';
 
 export default class View extends Component {
+    constructor(props) {
+        super(props);
+        const players = JSON.parse(localStorage.getItem('playersList'));
+        this.state = {
+            playersList: players
+        };
+    }
+
+    deletePlayer = (index) => {
+        let newPlayersList = this.state.playersList.slice();
+        newPlayersList.splice(index, 1);
+        localStorage.setItem('playersList', JSON.stringify(newPlayersList));
+        this.setState({playersList: newPlayersList});
+    }
 
     buildRosterList = () => {
-        const playersList = JSON.parse(localStorage.getItem('playersList'));
-        return playersList.map((player) => {
+        return this.state.playersList.map((player, index) => {
             return (
-                <li className="player-line" key={player.name}>
+                <li className="player-line" key={index}>
                     <span className="roster-entry-left">Name: {player.name}</span>
+                    <button onClick={this.deletePlayer.bind(null, index)} className="delete-button">Remove Player</button>
                     <span className="roster-entry-right">Position: {player.position}</span>
                 </li>
             )
